9bdb4ef52b537197dbce37961ffeea1b21af59f9
shamuso <shamuso23@gmail.com>
2025-08-13 23:43:17 -0500

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === "production" ? "/react-sandbox/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/idle-react/" : "/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
